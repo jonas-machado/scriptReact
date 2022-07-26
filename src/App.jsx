e8e6b4a8d2a5f1e9b90297dba0f1669077b89c2e
Jonas Machado Alves <jonas.m.a.1997@gmail.com>
2022-07-26 17:53:36 -0300

    vamo lá

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,27 @@ import * as yup from "yup";
 import "./App.css";
 
 function App() {
+
+  const validationLogin = yup.object().shape({
+    email: yup.string().email('Errrrrrrrrouuuuuuuu').required('Tem email não parça?'),
+    password: yup.string().min(8).required('Segurança da nasa, tem que colocar')
+  })
+
+  const handleClickLogin = (values) => console.log(values)
   return (
     <div className="container">
       <h1>Login</h1>
-      <Formik initialValues={{}}>
+      <Formik initialValues={{}} onSubmit={handleClickLogin} validationSchema={validationLogin}>
         <Form className="login-form">
           <div className="login-form-group">
             <Field name="email" className="form-field" placeholder="Email" />
+            <ErrorMessage component="span" name="email" className="form-error"/>
           </div>
+          <div className="login-form-group">
+            <Field name="password" className="form-field" placeholder="Senha" />
+            <ErrorMessage component="span" name="password" className="form-error"/>
+          </div>
+          <button className="button" type="submit">Login</button>
         </Form>
       </Formik>
       <h1></h1>
