64460b280df7134dab75e550f8d53412621bd8ce
Jonas Machado Alves <jonas.m.a.1997@gmail.com>
2022-08-01 17:50:31 -0300

    vai

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,22 @@ import "./App.css";
 
 function App() {
 
-  
 
   const validationLogin = yup.object().shape({
     email: yup.string().email('Errrrrrrrrouuuuuuuu').required('Tem email não parça?'),
-    password: yup.string().min(8).required('Segurança da nasa, tem que colocar')
+    password: yup.string().required('Tem que colocar')
   })
 
   const validationRegister = yup.object().shape({
     email: yup.string().email('Errrrrrrrrouuuuuuuu').required('Tem email não parça?'),
-    password: yup.string().min(8).required('Segurança da nasa, tem que colocar'),
+    password: yup.string().required('Tem que colocar'),
     confirmPassword: yup.string().oneOf([yup.ref('password'), null], 'Ta tirando com a minha cara?')
   })
   
   const handleClickRegister = (values) => console.log(values)
   const handleClickLogin = (values) => console.log(values)
   return (
-    <div id="containter" className="container">
+    <div id="container" className="container" >
       <h1>Login</h1>
       <Formik initialValues={{}} onSubmit={handleClickLogin} validationSchema={validationLogin}>
         <Form className="login-form">
@@ -33,10 +32,10 @@ function App() {
             <Field name="password" className="form-field" placeholder="Senha" />
             <ErrorMessage component="span" name="password" className="form-error"/>
           </div>
-          <input class="a" id="login" type="submit"/>
+          <input className="a" id="login" type="submit"/>
         </Form>
       </Formik>
-      <h1>Cadastro</h1>
+      <h1 className="h1Cadastro">Cadastro</h1>
       <Formik initialValues={{}} onSubmit={handleClickRegister} validationSchema={validationRegister}>
         <Form className="login-form">
           <div className="login-form-group">
@@ -51,7 +50,7 @@ function App() {
             <Field name="confirmPassword" className="form-field" placeholder="Confirme sua senha" />
             <ErrorMessage component="span" name="confirmPassword" className="form-error"/>
           </div>
-          <input class="a" id="login" type="submit"/>
+          <input className="a" id="login" type="submit"/>
         </Form>
       </Formik>
       <h1></h1>
