74af8b34696530f4edcfd930731f9365b183c38d
Jonas Machado Alves <jonas.m.a.1997@gmail.com>
2022-08-05 12:13:38 -0300

    passando de fase

---
+++ client/src/pages/LoginPage/index.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
+import "./index.css";
+import Axios from "axios"
+
+function LoginPage() {
+
+
+  const validationLogin = yup.object().shape({
+    email: yup.string().email('Errrrrrrrrouuuuuuuu').required('Tem email não parça?'),
+    password: yup.string().required('Tem que colocar')
+  })
+
+  const validationRegister = yup.object().shape({
+    email: yup.string().email('Errrrrrrrrouuuuuuuu').required('Tem email não parça?'),
+    password: yup.string().required('Tem que colocar'),
+    confirmPassword: yup.string().oneOf([yup.ref('password'), null], 'Ta tirando com a minha cara?')
+  })
+  
+  const handleClickLogin = values => {
+    Axios.post("http://127.0.0.1:3001/logar", {
+      email: values.email,
+      password: values.password,
+    }).then(result =>{
+      console.log(result)
+      console.log("funcionando")
+    }).catch(error => {
+      throw error;
+    });
+  }
+
+  const handleClickRegister = values => {
+    Axios.post("http://127.0.0.1:3001/register", {
+      email: values.email,
+      password: values.password,
+    }).then(result =>{
+      console.log(result)
+      console.log("funcionando")
+    })
+  }
+
+
+  return (
+    <div id="container" className="container" >
+      <h1>Login</h1>
+      <Formik initialValues={{email: "", password: "", confirmPassword: ""}} onSubmit={handleClickLogin} validationSchema={validationLogin}>
+        <Form className="login-form">
+          <div className="login-form-group">
+            <Field name="email" className="form-field" placeholder="Email" />
+            <ErrorMessage component="span" name="email" className="form-error"/>
+          </div>
+          <div className="login-form-group">
+            <Field name="password" className="form-field" placeholder="Senha" />
+            <ErrorMessage component="span" name="password" className="form-error"/>
+          </div>
+          <input className="a" id="login" type="submit" />
+        </Form>
+      </Formik>
+      <h1 className="h1Cadastro">Cadastro</h1>
+      <Formik initialValues={{email: "", password: "", confirmPassword: ""}} onSubmit={handleClickRegister} validationSchema={validationRegister}>
+        <Form className="login-form">
+          <div className="login-form-group">
+            <Field name="email" className="form-field" placeholder="Email" />
+            <ErrorMessage component="span" name="email" className="form-error"/>
+          </div>
+          <div className="login-form-group">
+            <Field name="password" className="form-field" placeholder="Senha" />
+            <ErrorMessage component="span" name="password" className="form-error"/>
+          </div>
+          <div className="login-form-group">
+            <Field name="confirmPassword" className="form-field" placeholder="Confirme sua senha" />
+            <ErrorMessage component="span" name="confirmPassword" className="form-error"/>
+          </div>
+          <input className="a" id="register" type="submit"/>
+        </Form>
+      </Formik>
+      <h1></h1>
+    </div>
+  );
+}
+
+export default LoginPage;
