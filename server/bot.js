1e08a2d4030fe7d33a196a258d208e0825207c99
Jonas <jonas.m.a.1997@gmail.com>
2022-09-01 00:34:40 -0300

    adding discord

--- server/bot.js
+++ server/bot.js
@@ -1,17 +1,17 @@
 require("dotenv").config()
 const { Client, GatewayIntentBits } = require('discord.js');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages]}); //create new client
+
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-  });
-  try{
-  client.on("message", msg => {
-    if (msg.content === "ping") {
-      msg.reply("pong");
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.TOKEN);
+
+  client.on('messageCreate', async interaction => {  
+  console.log(interaction)
+    if (interaction.content === 'quem eu amo mais que tudo?') {
+      await interaction.reply('Com certeza é a Amanda Isabelly Borba!');
     }
-  })
-}
-  catch{err => console.log(err)}
-  client.login(process.env.TOKEN);
-  console.log(process.env.TOKEN)
+  });
