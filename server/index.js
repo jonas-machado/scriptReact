a1ad1b40e8a6ad1a04a131660f605858ccc1235b
Jonas Machado Alves <jonas.m.a.1997@gmail.com>
2022-09-01 17:51:07 -0300

    finally?

--- server/index.js
+++ server/index.js
@@ -1,160 +1,242 @@
-require("dotenv").config()
+require("dotenv").config();
 
-const express = require("express")
-const app = express()
-const mysql = require("mysql2")
-const cors = require("cors")
-const bcrypt = require("bcrypt")
-const saltRounds = 10
-
+const express = require("express");
+const app = express();
+const mysql = require("mysql2");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const orion = require("solar-orionjs")({
-    server: "172.16.40.9",
-    port: 17778,
-    auth: {
-        username: "redes2020",
-        password: "OT#internet2018"
-    }
-})
+  server: "172.16.40.9",
+  port: 17778,
+  auth: {
+    username: "redes2020",
+    password: "OT#internet2018",
+  },
+});
 
-const bodyParser = require("body-parser")
-const cookieParser = require("cookie-parser")
-const session = require("express-session")
-const axios = require('axios')
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const axios = require("axios");
 
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+
+const { Client, GatewayIntentBits } = require("discord.js");
 
 const db = mysql.createPool({
-    host: "127.0.0.1",
-    user: "root",
-    password: "35796482",
-    database: "banco",
-})
+  host: "127.0.0.1",
+  user: "root",
+  password: "35796482",
+  database: "banco",
+});
 
-app.use(express.json())
-app.use(cors({
+app.use(express.json());
+app.use(
+  cors({
     origin: ["http://127.0.0.1:5173"],
     methods: ["GET", "POST"],
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
-app.use(cookieParser())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(session({
+app.use(
+  session({
     key: "userId",
     secret: process.env.SECRET_KEY,
     resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: 60 * 60 * 24 * 1000,
-    }
-}))
+      maxAge: 60 * 60 * 24 * 1000,
+    },
+  })
+);
 
 const verifyJWT = (req, res, next) => {
-    const token = req.headers["x-access-token"]
-    if(!token){
-        res.send("Precisa do token");
-    } 
-    else {
-        jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
-            if(err){
-                res.json({auth: false, msg: "falhou"});
-            } else {
-                req.userId = decoded.id;
-                next();
-            }
-        })
-    }
-}
-
-//discord bot
-
+  const token = req.headers["x-access-token"];
+  if (!token) {
+    res.send("Precisa do token");
+  } else {
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+      if (err) {
+        res.json({ auth: false, msg: "falhou" });
+      } else {
+        req.userId = decoded.id;
+        next();
+      }
+    });
+  }
+};
 
 //monitoramento
 app.get("/monitoramento", (req, res) => {
-    orion.query({query:`SELECT TOP 25 Message, EventTime, EventType FROM Orion.Events WHERE EventType LIKE "1" OR EventType LIKE "5" OR EventType LIKE "10" OR EventType LIKE "11" ORDER BY EventTime DESC`}, 
-    function (result){
-        res.send(result);
-    });})
+  orion.query(
+    {
+      query: `SELECT TOP 25 Message, EventTime, EventType FROM Orion.Events WHERE EventType LIKE "1" OR EventType LIKE "5" OR EventType LIKE "10" OR EventType LIKE "11" ORDER BY EventTime DESC`,
+    },
+    function (result) {
+      res.send(result);
+    }
+  );
+});
 
-const instance = axios.create({
-    host: "172.16.40.9:17778",
-    user: "redes2020",
-    password: "OT#internet2018",
-})
+//discord bot
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages,
+  ],
+}); //create new client
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.TOKEN);
+client.on("messageCreate", async (interaction) => {
+  if (interaction.content === "top") {
+    await interaction.reply("deu certo meu bom");
+  }
+});
+
+const adicionar = () => {
+  orion.query(
+    {
+      query: `SELECT TOP 15 Message, EventTime, EventType FROM Orion.Events WHERE EventType LIKE "1" OR EventType LIKE "5" OR EventType LIKE "10" OR EventType LIKE "11" ORDER BY EventTime DESC`,
+    },
+    function (result) {
+      let naovaiessamerda = [];
+      result.results.map((event) => {
+        naovaiessamerda.push(event.Message);
+      });
+
+      setInterval(() => {
+        orion.query(
+          {
+            query: `SELECT TOP 15 Message, EventTime, EventType FROM Orion.Events WHERE EventType LIKE "1" OR EventType LIKE "5" OR EventType LIKE "10" OR EventType LIKE "11" ORDER BY EventTime DESC`,
+          },
+          function (result) {
+            console.log(result.results[0].Message);
+            if (!naovaiessamerda.includes(result.results[0].Message)) {
+              console.log(result.results[0].Message);
+              client.channels.cache
+                .get(`994225969193828484`)
+                .send(`Esse: ${result.results[0].Message}`);
+              console.log(naovaiessamerda[0]);
+              naovaiessamerda.push(result.results[0].Message);
+            } else {
+              console.log("repetido");
+            }
+          }
+        );
+      }, 10000);
+    }
+  );
+};
+adicionar();
+//   setInterval(() =>{
+//   orion.query({query:`SELECT TOP 15 Message, EventTime, EventType FROM Orion.Events WHERE EventType LIKE "1" OR EventType LIKE "5" OR EventType LIKE "10" OR EventType LIKE "11" ORDER BY EventTime DESC`},
+//   function (result){
+//     const message = result.results[0].Message
+//     const time = result.results[0].EventTime
+//     const messagePast = result.results[1].Message
+//     const verify = result.results.map((event) =>{
+//         state.push(event.Message)
+//     })
+//     if(!message == state[0]){
+//     client.channels.cache.get(`994225969193828484`).send(`${message} // ${time.substr(0, 19).split("T").join(" ")}`)
+//     console.log(state[0])
+//     state.push(message)
+//     }else{console.log("repetido")}
+//   })
+// }, 10000)
 
 app.get("/isUserAuth", verifyJWT, (req, res) => {
-    res.send("Autenticado")
-})
+  res.send("Autenticado");
+});
 
-app.get("/login", (req, res) =>{
-    if(req.session.user) {
-        res.send({
-            loggedIn: true,
-            user: req.session.user,
-        })
-    }else{res.send({loggedIn: false})}
-})
+app.get("/login", (req, res) => {
+  if (req.session.user) {
+    res.send({
+      loggedIn: true,
+      user: req.session.user,
+    });
+  } else {
+    res.send({ loggedIn: false });
+  }
+});
 
 app.post("/login", (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    db.query("SELECT * FROM banco.usuarios WHERE email = ?", 
-        email, (err, result) => {
-            if (err) {
-                res.send(err)
-            }
-            if(result.length > 0){
-                bcrypt.compare(password, result[0].password, (err, response) => {
-                    if(response){
-                        const id = result[0].idusuarios
-                        const token = jwt.sign({id}, process.env.SECRET_KEY, {
-                            expiresIn: 300,
-                        })
+  const email = req.body.email;
+  const password = req.body.password;
+  db.query(
+    "SELECT * FROM banco.usuarios WHERE email = ?",
+    email,
+    (err, result) => {
+      if (err) {
+        res.send(err);
+      }
+      if (result.length > 0) {
+        bcrypt.compare(password, result[0].password, (err, response) => {
+          if (response) {
+            const id = result[0].idusuarios;
+            const token = jwt.sign({ id }, process.env.SECRET_KEY, {
+              expiresIn: 300,
+            });
 
-                        req.session.user = result
+            req.session.user = result;
 
-
-                        res.json({auth: true, token: token, result: result})
-
-                    } else {
-                        res.send({msg: "Alguma coisa deu errada"})
-                    }
-                })
-            } else {res.json({auth: false, msg: "Alguma coisa deu errada"})}
-    })
-})
+            res.json({ auth: true, token: token, result: result });
+          } else {
+            res.send({ msg: "Alguma coisa deu errada" });
+          }
+        });
+      } else {
+        res.json({ auth: false, msg: "Alguma coisa deu errada" });
+      }
+    }
+  );
+});
 
 app.post("/register", (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    db.query("SELECT * FROM banco.usuarios WHERE email = ?", [email], (err, response) => {
-        if(err) {
-            res.send(err)
-        }
-        if (response.length == 0){
-            bcrypt.hash(password, saltRounds, (err, hash) => {
-                if(err){
-                    console.log(err)
-                }
-                db.query(
-                    "INSERT INTO banco.usuarios (email, password) VALUES (?, ?)", [email, hash],
-                    (err, result) => {
-                        if (err) {
-                            res.send(err)
-                        }
-                        res.send({msg: "Cadastrado com sucesso"})
-                    }
-                )
-            })
-            
-        } else {res.send({msg: "Usuário já cadastrado"})}
-    })
-})
-
-
+  const email = req.body.email;
+  const password = req.body.password;
+  db.query(
+    "SELECT * FROM banco.usuarios WHERE email = ?",
+    [email],
+    (err, response) => {
+      if (err) {
+        res.send(err);
+      }
+      if (response.length == 0) {
+        bcrypt.hash(password, saltRounds, (err, hash) => {
+          if (err) {
+            console.log(err);
+          }
+          db.query(
+            "INSERT INTO banco.usuarios (email, password) VALUES (?, ?)",
+            [email, hash],
+            (err, result) => {
+              if (err) {
+                res.send(err);
+              }
+              res.send({ msg: "Cadastrado com sucesso" });
+            }
+          );
+        });
+      } else {
+        res.send({ msg: "Usuário já cadastrado" });
+      }
+    }
+  );
+});
 
 app.listen(3001, () => {
-    console.log("Rodando na porta 3001")
-})
+  console.log("Rodando na porta 3001");
+});
