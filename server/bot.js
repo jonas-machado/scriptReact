280db792470d3cac8902043b4eb032d42b104d58
Jonas Machado Alves <jonas.m.a.1997@gmail.com>
2022-09-14 18:07:18 -0300

    finalizado

--- server/bot.js
+++ server/bot.js
@@ -34,7 +34,7 @@ client.on("messageCreate", (interaction) => {
     orion.query(
       {
         query: `
-          SELECT TOP 1 
+          SELECT TOP 10 
           DAY(EventTime) AS DayTime, 
           MONTH(EventTime) AS MonthTime, 
           year(EventTime) AS YearTime, 
@@ -72,158 +72,263 @@ client.on("messageCreate", (interaction) => {
       },
       function (result) {
         console.log(result);
-        const exampleEmbed = new EmbedBuilder()
-          .setColor(0xff0000)
-          .setTitle(result.results[0].NodeName);
-        client.channels.cache
-          .get(`994225969193828484`)
-          .send({ embeds: [exampleEmbed] });
+        try {
+          const events = new EmbedBuilder()
+            .setColor(
+              result.results[0].EventType == 1 ||
+                result.results[0].EventType == 10
+                ? 0xff0000
+                : 0x32cd32
+            )
+            .setTitle(result.results[0].NodeName.replace(/_/g, " "))
+            .setDescription(result.results[0].Message)
+            .addFields(
+              { name: "IP", value: result.results[0].IPAddress, inline: true },
+              result.results[10]?.Department
+                ? {
+                    name: "TIPO",
+                    value: result.results[10].Department,
+                    inline: true,
+                  }
+                : {
+                    name: "\u200B",
+                    value: "\u200B",
+                    inline: true,
+                  },
+              result.results[0].POP_ID || result.results[0].Location
+                ? {
+                    name: "LOCAL",
+                    value: `${
+                      result.results[0].POP_ID
+                        ? result.results[0].POP_ID + "."
+                        : ""
+                    }  ${
+                      result.results[0].Location
+                        ? result.results[0].Location + "."
+                        : ""
+                    }`,
+                  }
+                : {
+                    name: "\u200B",
+                    value: "\u200B",
+                    inline: true,
+                  },
+              result.results[0].City
+                ? {
+                    name: "CIDADE",
+                    value: result.results[0].City,
+                  }
+                : {
+                    name: "\u200B",
+                    value: "\u200B",
+                    inline: true,
+                  }
+            )
+            .setTimestamp(
+              `${result.results[i].Message} \nData: ${(
+                "0" + result.results[i].DayTime
+              ).slice(-2)}/${("0" + result.results[i].MonthTime).slice(-2)}/${
+                result.results[i].YearTime
+              } ${("0" + result.results[i].HourTime).slice(-2)}:${(
+                "0" + result.results[i].MinuteTime
+              ).slice(-2)}:${("0" + result.results[i].SecondTime).slice(-2)}\n`
+            );
+          interaction.reply({ embeds: [events] });
+        } catch (err) {
+          console.log(err);
+        }
       }
     );
   }
 });
 
-// const adicionar = () => {
-//   orion.query(
-//     {
-//       query: `
-//       SELECT TOP 15
-//       DAY(EventTime) AS DayTime,
-//       MONTH(EventTime) AS MonthTime,
-//       year(EventTime) AS YearTime,
-//       HOUR(EventTime) AS HourTime,
-//       MINUTE(EventTime) AS MinuteTime,
-//       SECOND(EventTime) AS SecondTime,
-//       Message,
-//       EventTime,
-//       EventID,
-//       EventType,
-//       n.NodeID,
-//       ncp.NodeID,
-//       IPAddress,
-//       NodeName,
-//       Location,
-//       NodeDescription,
-//       POP_ID,
-//       City,
-//       Department,
-//       City
-//       FROM
-//       Orion.Events AS e,
-//       Orion.Nodes AS n,
-//       Orion.NodesCustomProperties AS ncp
-//       WHERE
-//       EventType LIKE "1" AND networknode=n.nodeid AND networknode=ncp.nodeid
-//       OR
-//       EventType LIKE "5" AND networknode=n.nodeid AND networknode=ncp.nodeid
-//       OR
-//       EventType LIKE "10" AND networknode=n.nodeid AND networknode=ncp.nodeid
-//       OR
-//       EventType LIKE "11" AND networknode=n.nodeid AND networknode=ncp.nodeid
-//       ORDER BY EventTime DESC;
-//                   `,
-//       //query: `SELECT TOP 15 GETDATE() AS Time, Message, EventTime, EventType FROM Orion.Events WHERE EventType LIKE "1" OR EventType LIKE "5" OR EventType LIKE "10" OR EventType LIKE "11" ORDER BY EventTime DESC`,
-//     },
-//     function (result) {
-//       let IDEvent = [];
-//       result.results.map((event) => {
-//         try {
-//           let evento = {
-//             description: event.Message,
-//             time: `${("0" + event.DayTime).slice(-2)}/${(
-//               "0" + event.MonthTime
-//             ).slice(-2)}/${event.YearTime} ${("0" + event.HourTime).slice(
-//               -2
-//             )}:${("0" + event.MinuteTime).slice(-2)}:${(
-//               "0" + event.SecondTime
-//             ).slice(-2)}`,
-//           };
-//           IDEvent.push(event.EventID);
-//         } catch (err) {
-//           console.log(err);
-//         }
-//       });
-//       setInterval(() => {
-//         orion.query(
-//           {
-//             query: `
-//             SELECT TOP 15
-//             DAY(EventTime) AS DayTime,
-//             MONTH(EventTime) AS MonthTime,
-//             year(EventTime) AS YearTime,
-//             HOUR(EventTime) AS HourTime,
-//             MINUTE(EventTime) AS MinuteTime,
-//             SECOND(EventTime) AS SecondTime,
-//             Message,
-//             EventTime,
-//             EventID,
-//             EventType,
-//             n.NodeID,
-//             ncp.NodeID,
-//             IPAddress,
-//             NodeName,
-//             Location,
-//             NodeDescription,
-//             POP_ID,
-//             City,
-//             Department,
-//             City
-//             FROM
-//             Orion.Events AS e,
-//             Orion.Nodes AS n,
-//             Orion.NodesCustomProperties AS ncp
-//             WHERE
-//             EventType LIKE "1" AND networknode=n.nodeid AND networknode=ncp.nodeid
-//             OR
-//             EventType LIKE "5" AND networknode=n.nodeid AND networknode=ncp.nodeid
-//             OR
-//             EventType LIKE "10" AND networknode=n.nodeid AND networknode=ncp.nodeid
-//             OR
-//             EventType LIKE "11" AND networknode=n.nodeid AND networknode=ncp.nodeid
-//             ORDER BY EventTime DESC;
-//                   `,
-//             //query: `SELECT TOP 15 GETDATE() AS Time, Message, EventTime, EventType FROM Orion.Events WHERE EventType LIKE "1" OR EventType LIKE "5" OR EventType LIKE "10" OR EventType LIKE "11" ORDER BY EventTime DESC`,
-//             //query: `SELECT Message, EventTime, GETDATE() AS [GetDate], GETUTCDATE() AS [getUTCDate] FROM Orion.Events`,
-//           },
-//           function (result) {
-//             let eventNew = [];
-//             result.results.map((event) => {
-//               let eventN = {
-//                 description: event.Message,
-//                 time: `${("0" + event.DayTime).slice(-2)}/${(
-//                   "0" + event.MonthTime
-//                 ).slice(-2)}/${event.YearTime} ${("0" + event.HourTime).slice(
-//                   -2
-//                 )}:${("0" + event.MinuteTime).slice(-2)}:${(
-//                   "0" + event.SecondTime
-//                 ).slice(-2)}`,
-//                 id: event.EventID,
-//               };
-//               eventNew.unshift(eventN);
-//             });
-//             try {
-//               for (let i = 0; i < eventNew.length; i++) {
-//                 if (!IDEvent.includes(eventNew[i].id)) {
-//                   client.channels.cache
-//                     .get(`994225969193828484`)
-//                     .send(
-//                       `${eventNew[i].description} \nData: ${eventNew[i].time}\n`
-//                     );
-//                   IDEvent.unshift(eventNew[i].id);
-//                   IDEvent.pop();
-//                   console.log(eventNew[i].description);
-//                   eventNew = [];
-//                 } else {
-//                   console.log("repetido");
-//                 }
-//               }
-//             } catch (err) {
-//               console.log(err);
-//             }
-//           }
-//         );
-//       }, 10000);
-//     }
-//   );
-// };
-// adicionar();
+const adicionar = () => {
+  orion.query(
+    {
+      query: `
+      SELECT TOP 10 
+          DAY(EventTime) AS DayTime, 
+          MONTH(EventTime) AS MonthTime, 
+          year(EventTime) AS YearTime, 
+          HOUR(EventTime) AS HourTime, 
+          MINUTE(EventTime) AS MinuteTime, 
+          SECOND(EventTime) AS SecondTime, 
+          Message, 
+          EventTime, 
+          EventID,
+          EventType,
+          n.NodeID,
+          ncp.NodeID AS NCPNode,
+          IPAddress,
+          NodeName,
+          Location,
+          NodeDescription,
+          POP_ID,
+          City,
+          Department
+          FROM  
+          Orion.Events AS e, 
+          Orion.Nodes AS n,
+          Orion.NodesCustomProperties AS ncp
+          WHERE 
+          EventType LIKE "1" AND networknode=n.nodeid AND networknode=ncp.nodeid
+          OR 
+          EventType LIKE "5" AND networknode=n.nodeid AND networknode=ncp.nodeid
+          OR 
+          EventType LIKE "10" AND networknode=n.nodeid AND networknode=ncp.nodeid
+          OR 
+          EventType LIKE "11" AND networknode=n.nodeid AND networknode=ncp.nodeid
+          ORDER BY EventTime DESC;
+                  `,
+      //query: `SELECT TOP 15 GETDATE() AS Time, Message, EventTime, EventType FROM Orion.Events WHERE EventType LIKE "1" OR EventType LIKE "5" OR EventType LIKE "10" OR EventType LIKE "11" ORDER BY EventTime DESC`,
+    },
+    function (result) {
+      let IDEvent = [];
+      result.results.map((event) => {
+        IDEvent.push(event.EventID);
+      });
+      console.log(IDEvent);
+      setInterval(() => {
+        orion.query(
+          {
+            query: `
+            SELECT TOP 10 
+          DAY(EventTime) AS DayTime, 
+          MONTH(EventTime) AS MonthTime, 
+          year(EventTime) AS YearTime, 
+          HOUR(EventTime) AS HourTime, 
+          MINUTE(EventTime) AS MinuteTime, 
+          SECOND(EventTime) AS SecondTime, 
+          Message, 
+          EventTime, 
+          EventID,
+          EventType,
+          n.NodeID,
+          ncp.NodeID AS NCPNode,
+          IPAddress,
+          NodeName,
+          Location,
+          NodeDescription,
+          POP_ID,
+          City,
+          Department
+          FROM  
+          Orion.Events AS e, 
+          Orion.Nodes AS n,
+          Orion.NodesCustomProperties AS ncp
+          WHERE 
+          EventType LIKE "1" AND networknode=n.nodeid AND networknode=ncp.nodeid
+          OR 
+          EventType LIKE "5" AND networknode=n.nodeid AND networknode=ncp.nodeid
+          OR 
+          EventType LIKE "10" AND networknode=n.nodeid AND networknode=ncp.nodeid
+          OR 
+          EventType LIKE "11" AND networknode=n.nodeid AND networknode=ncp.nodeid
+          ORDER BY EventTime DESC;
+                  `,
+            //query: `SELECT TOP 15 GETDATE() AS Time, Message, EventTime, EventType FROM Orion.Events WHERE EventType LIKE "1" OR EventType LIKE "5" OR EventType LIKE "10" OR EventType LIKE "11" ORDER BY EventTime DESC`,
+            //query: `SELECT Message, EventTime, GETDATE() AS [GetDate], GETUTCDATE() AS [getUTCDate] FROM Orion.Events`,
+          },
+          function (result) {
+            try {
+              for (let i = 0; i < result.results.length; i++) {
+                if (!IDEvent.includes(result.results[i].EventID)) {
+                  const events = new EmbedBuilder()
+                    .setColor(
+                      result.results[i].EventType == 1 ||
+                        result.results[i].EventType == 10
+                        ? 0xff0000
+                        : 0x32cd32
+                    )
+                    .setTitle(result.results[i].NodeName.replace(/_/g, " "))
+                    .setDescription(result.results[i].Message)
+                    .addFields(
+                      {
+                        name: "IP",
+                        value: result.results[i].IPAddress,
+                        inline: true,
+                      },
+                      result.results[i]?.Department
+                        ? {
+                            name: "TIPO",
+                            value: result.results[i].Department,
+                            inline: true,
+                          }
+                        : {
+                            name: "\u200B",
+                            value: "\u200B",
+                            inline: true,
+                          },
+                      {
+                        name: "DIA/HORA",
+                        value: `${("0" + result.results[i].DayTime).slice(
+                          -2
+                        )}/${("0" + result.results[i].MonthTime).slice(-2)}/${
+                          result.results[i].YearTime
+                        } ${("0" + result.results[i].HourTime).slice(-2)}:${(
+                          "0" + result.results[i].MinuteTime
+                        ).slice(-2)}:${(
+                          "0" + result.results[i].SecondTime
+                        ).slice(-2)}`,
+                        inline: true,
+                      },
+                      result.results[i].POP_ID || result.results[i].Location
+                        ? {
+                            name: "LOCAL",
+                            value: `${
+                              result.results[i].POP_ID
+                                ? result.results[i].POP_ID + "."
+                                : ""
+                            }  ${
+                              result.results[i].Location
+                                ? result.results[i].Location + "."
+                                : ""
+                            }`,
+                          }
+                        : {
+                            name: "\u200B",
+                            value: "\u200B",
+                            inline: true,
+                          },
+                      result.results[i].City
+                        ? {
+                            name: "CIDADE",
+                            value: result.results[i].City,
+                          }
+                        : {
+                            name: "\u200B",
+                            value: "\u200B",
+                            inline: true,
+                          }
+                    );
+                  client.channels.cache
+                    .get(`994225969193828484`)
+                    .send({ embeds: [events] });
+                  // .send(
+                  //   `${result.results[i].Message} \nData: ${(
+                  //     "0" + result.results[i].DayTime
+                  //   ).slice(-2)}/${("0" + result.results[i].MonthTime).slice(
+                  //     -2
+                  //   )}/${result.results[i].YearTime} ${(
+                  //     "0" + result.results[i].HourTime
+                  //   ).slice(-2)}:${("0" + result.results[i].MinuteTime).slice(
+                  //     -2
+                  //   )}:${("0" + result.results[i].SecondTime).slice(-2)}\n`
+                  // );
+                  IDEvent.unshift(result.results[i].EventID);
+                  IDEvent.pop();
+                  console.log(result.results[i].Message);
+                } else {
+                  console.log("repetido");
+                }
+              }
+            } catch (err) {
+              console.log(err);
+            }
+          }
+        );
+      }, 10000);
+    }
+  );
+};
+adicionar();
