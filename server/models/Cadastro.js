504ade273697a1e662170fcc58185cebdeba0a59
Jonas <jonas.m.a.1997@gmail.com>
2022-12-02 01:18:37 -0300

    starting

---
+++ server/models/Cadastro.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+const Cadastro = sequelize.define("Cadastro", {
+    idusuarios: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    } 
+  })
+
+  return Cadastro
+}
