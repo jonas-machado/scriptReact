eebc0538a7d3c99c803a4195e2066a45c5ef5820
Jonas Machado Alves <jonas.m.a.1997@gmail.com>
2022-12-05 17:39:06 -0300

    doing

--- server/models/Cadastro.js
+++ server/models/Cadastro.js
@@ -1,23 +1,23 @@
 module.exports = (sequelize, DataTypes) => {
-const Cadastro = sequelize.define("Cadastro", {
+  const Cadastro = sequelize.define("Cadastro", {
     idusuarios: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
     email: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    } 
-  })
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
 
-  return Cadastro
-}
+  return Cadastro;
+};
