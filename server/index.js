affe52ff35f64436566495b230e3b1690313993d
Jonas Machado Alves <jonas.m.a.1997@gmail.com>
2022-12-12 17:36:59 -0300

    done

--- server/index.js
+++ server/index.js
@@ -5,28 +5,18 @@ const app = express();
 const cors = require("cors");
 const db = require("./models");
 
-const orion = require("solar-orionjs")({
-  server: "172.16.40.9",
-  port: 17778,
-  auth: {
-    username: "redes2020",
-    password: "OT#internet2018",
-  },
-});
-
 const jwt = require("jsonwebtoken");
 
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://127.0.0.1:3000"],
     methods: ["GET", "POST"],
     credentials: true,
   })
 );
 
 const verifyJWT = (req, res, next) => {
-  const token = req.cookies.access_token;
   if (!token) {
     res.send("Precisa do token");
   } else {
